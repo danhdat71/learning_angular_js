cf3c2c444f51b065ad06f1749bc316a5437844cf
Bap-datd <datd@bap.jp>
2024-08-28 22:43:56 +0700

    Set validate

--- directive.js
+++ directive.js
@@ -27,34 +27,30 @@ app.directive('customOnChange', function () {
     };
 });
 
-app.directive('customInputValidImage', function(){
+app.directive('customInputValidImage', function () {
     return {
         restrict: 'A',
         require: 'ngModel',
-        link: function(scope, elem, attrs, ngModel) {
-            var validFormats = ['jpg','jpeg','png'];
-            elem.on('change', function () {
-                validImage(false);
+        link: function (scope, element, attrs, ngModel) {
+            var maxFileSize = parseInt(attrs.customInputValidImage);
+
+            function validateFileSize(value) {
+                if (value.size > maxFileSize) {
+                    ngModel.$setValidity('validExtension', false);
+                } else {
+                    ngModel.$setValidity('validExtension', true);
+                }
+
+                return value;
+            }
+
+            ngModel.$parsers.push(validateFileSize);
+
+            element.on('change', function () {
                 scope.$apply(function () {
-                    ngModel.$render();
+                    ngModel.$setViewValue(element[0].files[0]);
                 });
             });
-            ngModel.$render = function () {
-                ngModel.$setViewValue(elem.val());
-            };
-            function validImage(bool) {
-                ngModel.$setValidity('validExtension', bool);
-            }
-            ngModel.$parsers.push(function(value) {
-                var ext = value.substr(value.lastIndexOf('.')+1);
-                if(ext=='') return;
-                if(validFormats.indexOf(ext) == -1){
-                    return value;
-                }
-                validImage(true);
-                return value;
-            });
-            
         }
     }
 });
